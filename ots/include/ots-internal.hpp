9f7ec7631e0db364151751d62dcf315971513a1e
DiosDelRayo <diosdelrayo@localhost>
2024-12-08 13:06:34 -0600

    deactivate easylogging

--- ots/include/ots-internal.hpp
+++ ots/include/ots-internal.hpp
@@ -10,21 +10,10 @@
  */
 
 #include "ots.hpp"
-#include "ea_config.h"
 #include "crypto/crypto.h"
 #include "mnemonics/electrum-words.h"
 #include "cryptonote_basic/cryptonote_basic.h"
 #include "cryptonote_core/cryptonote_core.h"
-
-#define ELPP_THREAD_SAFE
-#define ELPP_NO_DEFAULT_LOG_FILE
-#define ELPP_CUSTOM_COUT std::cout
-#define ELPP_STACKTRACE_ON_CRASH
-#define ELPP_LOGGING_FLAGS_FILENAME_DISPLAY
-#define ELPP_DISABLE_PERFORMANCE_TRACKING
-
-#include "easylogging++.h"
-INITIALIZE_EASYLOGGINGPP
 
 namespace ots {
     crypto::secret_key keyFromHash(const std::vector<unsigned char>& data);
