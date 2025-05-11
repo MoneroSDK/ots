6bf08a4961ab76eb20b27226e55b424597242dfa
DiosDelRayo <diosdelrayo@localhost>
2025-05-11 14:08:15 -0600

    finished bu redundant tests, do not like it, but not worth to waste time on it now either

--- ots/tests/c-abi/wipeable_string.c
+++ ots/tests/c-abi/wipeable_string.c
@@ -1,6 +1,81 @@
 #include <check.h>
 #include "../include/ots.h"
 
+/**
+ * @file wipeable_string.c
+ * @note Tests for WipeableString are a bit redudant
+ * @todo Should maybe restructure the tests, but all covert, but
+ *       redundant, ugly but not hurting (restructure when time allows,
+ *       so probably never?)
+ */
+
+START_TEST(test_ots_wipeable_string_create)
+{
+    char* test_str = "Wipeable Secret";
+    ots_result_t* result = ots_wipeable_string_create(test_str);
+    ck_assert(ots_result_is_wipeable_string(result));
+    ots_handle_t* ws = ots_result_handle(result);
+    ots_free_result(&result);
+    ck_assert_str_eq(ots_wipeable_string_c_str(ws), test_str);
+    ots_free_handle(&ws);
+}
+END_TEST
+
+START_TEST(test_ots_wipeable_string_compare)
+{
+    char* test_strings[5];
+    test_strings[0] = "AAAAAA";
+    test_strings[1] = "BBBBBB";
+    test_strings[2] = "CCCCCC";
+    test_strings[3] = "DDDDDD";
+    test_strings[4] = "EEEEEE";
+    ots_handle_t* ws[5];
+    for(size_t i = 0; i < 5; i++) {
+        ots_result_t* result = ots_wipeable_string_create(test_strings[i]);
+        ck_assert(ots_result_is_wipeable_string(result));
+        ws[i] = ots_result_handle(result);
+        ots_free_result(&result);
+    }
+    ots_result_t* result = ots_wipeable_string_create(ots_wipeable_string_c_str(ws[2]));
+    ck_assert(ots_result_is_wipeable_string(result));
+    ots_handle_t* ws_ref = ots_result_handle(result);
+    ots_free_result(&result);
+    for(size_t i = 0; i < 5; i++) {
+        result = ots_wipeable_string_compare(ws[i], ws_ref);
+        ck_assert(ots_result_is_comparison(result));
+        ck_assert(ots_result_is_equal(result) == (i==2));
+        switch(i) {
+            case 0:
+            case 1:
+                ck_assert_int_lt(ots_result_comparison(result), 0);
+                break;
+            case 2:
+                ck_assert_int_eq(ots_result_comparison(result), 0);
+                break;
+            case 3:
+            case 4:
+                ck_assert_int_gt(ots_result_comparison(result), 0);
+                break;
+        }
+    }
+}
+END_TEST
+
+START_TEST(test_ots_wipeable_string_c_str)
+{
+    char* test_str = "Wipeable Secret";
+    ots_result_t* result = ots_wipeable_string_create(test_str);
+    ck_assert(ots_result_is_wipeable_string(result));
+    ots_handle_t* ws = ots_result_handle(result);
+    ots_free_result(&result);
+    const char* c_str = ots_wipeable_string_c_str(ws);
+    ck_assert_str_eq(c_str, test_str);
+    ots_free_handle(&ws); // wipes c_str, too
+    ck_assert_str_ne(c_str, test_str);
+}
+END_TEST
+
+
 START_TEST(test_wipeable_string)
 {
     // define test strings
@@ -61,6 +136,9 @@ Suite* wipeable_string_suite(void)
     // Core functionality
     TCase* tc_core = tcase_create("Core");
     tcase_add_test(tc_core, test_wipeable_string);
+    tcase_add_test(tc_core, test_ots_wipeable_string_create);
+    tcase_add_test(tc_core, test_ots_wipeable_string_compare);
+    tcase_add_test(tc_core, test_ots_wipeable_string_c_str);
     suite_add_tcase(s, tc_core);
     return s;
 }
